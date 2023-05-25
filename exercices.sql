022b1730b2401737ea49905be3996726e7ca9253
Benjamin Louis <louisbenetval@orange.fr>
2023-05-25 18:34:19 +0200

    MAJ

--- exercices.sql
+++ exercices.sql
@@ -2,12 +2,12 @@
 
 exercice 1 a -- Donner la liste des titres des représentations 
 
-SELECT r.titre_representation                       -- SELECT: nom de la colone ou des colones Utilisé pour lire des données issues de la base de données en retournant des enregistrements dans un tableau de résultat.
-FROM representation r                               -- FROM: nom de la table Indique le lieu dans lequel les données à lire se trouvent
+SELECT r.titre_representation                                                       -- SELECT: nom de la colone ou des colones Utilisé pour lire des données issues de la base de données en retournant des enregistrements dans un tableau de résultat.
+FROM representation r                                                               -- FROM: nom de la table Indique le lieu dans lequel les données à lire se trouvent
 
 exercice 1 b-- Donner la liste des titres des représentations ayant lieu à l'opéra Bastille
 
-SELECT r.titre_representation                       -- La commande WHERE dans une requête SQL permet d’extraire les lignes d’une base de données qui respectent une condition.
+SELECT r.titre_representation                                                       -- La commande WHERE dans une requête SQL permet d’extraire les lignes d’une base de données qui respectent une condition.
 FROM representation r
 WHERE r.lieu = "Opéra Bastille"
 
@@ -15,27 +15,27 @@ exercice 1 c-- Donner la liste des noms des musiciens et des titres des représe
 
 SELECT m.nom, r.titre_representation
 FROM musicien m, representation r
-WHERE m.representation_id = r.num_representation    -- Faire 1 jointure quand on utilise 2 tables (2 pour 3 tables ...) Elles se font entre la clé primaire et la clé étrangère
+WHERE m.representation_id = r.num_representation                                    -- Faire 1 jointure quand on utilise 2 tables (2 pour 3 tables ...) Elles se font entre la clé primaire et la clé étrangère
 
 exercice 1 d-- Donner la liste des titres des représentations, les lieux et les tarifs pour la journée du 14/09/2014.
 
 SELECT r.titre_representation, p.tarif
 FROM representation r, programmer p
 WHERE r.num_representation = p.representation_id
-AND p.date = '2014-09-14'                           -- AND Remplace WHERE car on ne peut en mettre q'un
+AND p.date = '2014-09-14'                                                           -- AND Remplace WHERE car on ne peut en mettre q'un
 
 exercice 2 a-- Quel est le nombre total d'étudiants ?
 
-SELECT COUNT(e.n_etudiant)                          -- En SQL, la fonction d’agrégation COUNT() permet de compter le nombre d’enregistrement dans une table.
+SELECT COUNT(e.n_etudiant)                                                          -- En SQL, la fonction d’agrégation COUNT() permet de compter le nombre d’enregistrement dans une table.
 FROM etudiant e
 
 exercice 2 b-- Quelles sont, parmi l'ensemble des notes, la note la plus haute et la note la plus basse ?
 
-SELECT MAX(e.note), MIN(e.note)                     -- MAX() et MIN() sont des fonctions d’agrégation permettent de retourner la valeur maximale et minimale d’une colonne dans un set d’enregistrement.
+SELECT MAX(e.note), MIN(e.note)                                                     -- MAX() et MIN() sont des fonctions d’agrégation permettent de retourner la valeur maximale et minimale d’une colonne dans un set d’enregistrement.
 FROM evaluer e
 SELECT n_etudiant, MAX(e.note), MIN(e.note)
 FROM evaluer e
-GROUP BY n_etudiant                                 -- GROUP BY Permet d'afficher la note max et min de chaque numéro d'étudiant
+GROUP BY n_etudiant                                                                 -- GROUP BY Permet d'afficher la note max et min de chaque numéro d'étudiant
 
 exercice 2 c-- Quelles sont les moyennes de chaque étudiant dans chacune des matières ? (utilisez CREATE VIEW)
 
@@ -44,8 +44,7 @@ SELECT etu.n_etudiant, etu.nom, etu.prenom, m.libellemat, AVG(eva.note) AS moyEt
 FROM etudiant etu, evaluer eva, matiere m                                           -- AVG La fonction d’agrégation AVG() dans le langage SQL permet de calculer une valeur moyenne sur un ensemble d’enregistrement de type numérique et non nul.
 WHERE eva.n_etudiant = etu.n_etudiant                                               -- AS est un alias
 AND eva.codemat = m.codemat                                                         -- etu - eva - m sont aussi des alias
-
-GROUP BY etu.n_etudiant, etu.nom, etu.prenom, m.libellemat                          --GROUP BY Pour grouper plusieurs résultats et utiliser une fonction de totaux sur un groupe de résultat. 
+GROUP BY etu.n_etudiant, etu.nom, etu.prenom, m.libellemat                          -- GROUP BY Pour grouper plusieurs résultats et utiliser une fonction de totaux sur un groupe de résultat. 
 
 exercice 2 d-- Quelles sont les moyennes par matière ? (cf. question c)
 
@@ -71,8 +70,12 @@ GROUP BY etu.nom, etu.prenom
 exercice 2 f-- Quelle est la moyenne générale de la promotion ? (cf. question e)
 
 CREATE OR REPLACE VIEW moyenne_promo AS
-SELECT AVG(evaluer.note)
+SELECT AVG(evaluer.note) AS moyennePromo
 FROM evaluer
+
+exercice 2 g--Quels sont les étudiants qui ont une moyenne générale supérieure ou égale à la moyenne générale de la promotion ? (cf. question e)
+
+
 
 
 
